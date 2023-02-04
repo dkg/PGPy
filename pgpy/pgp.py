06709eb88d881fbc77af17b8e29515be8d6f8eb4
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2023-02-03 23:00:33 -0500

    allow aead_ciphersuites preferences when certifying

--- pgpy/pgp.py
+++ pgpy/pgp.py
@@ -2226,6 +2226,7 @@ class PGPKey(Armorable, ParentRef, PGPObject):
             primary_uid = prefs.pop('primary', None)
             attested_certifications = prefs.pop('attested_certifications', [])
             features = prefs.pop('features', Features.pgpy_features)
+            aead_ciphersuites = prefs.pop('aead_ciphersuites', [])
 
             if key_expires is not None:
                 # key expires should be a timedelta, so if it's a datetime, turn it into a timedelta
@@ -2282,6 +2283,9 @@ class PGPKey(Armorable, ParentRef, PGPObject):
                             'a bytes object of size {:d}; ignoring'.format(sig.hash_algorithm.digest_size)
                         )
                 sig._signature.subpackets.addnew('AttestedCertifications', hashed=True, attested_certifications=b''.join(sorted(attestations)))
+
+            if aead_ciphersuites:
+                sig._signature.subpackets.addnew('PreferredAEADCiphersuites', hashed=True, preferred_ciphersuites=aead_ciphersuites)
 
         else:
             # signature options that only make sense in non-self-certifications
