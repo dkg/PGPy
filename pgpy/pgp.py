1a761adae7225593c72c921fb3c3c8ac42fcd563
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2023-05-13 03:30:27 -0400

    Consolidate fingerprint-bearing subpackets
    IntendedRecipient and IssuerFingerprint subpackets have the same wire formats,
    just different semantics.  Consolidate the codebase here.
    
    Note that a freshly-created subpacket like this will emit an all-zeros
    fingerprint if you ask it for one, but will refuse to render itself
    into a wireformat.

--- pgpy/pgp.py
+++ pgpy/pgp.py
@@ -2050,7 +2050,7 @@ class PGPKey(Armorable, ParentRef, PGPObject):
 
         if prefs.pop('include_issuer_fingerprint', True):
             if isinstance(self._key, PrivKeyV4):
-                sig._signature.subpackets.addnew('IssuerFingerprint', hashed=True, _version=4, _issuer_fpr=self.fingerprint)
+                sig._signature.subpackets.addnew('IssuerFingerprint', hashed=True, _version=4, _fpr=self.fingerprint)
 
         # place the subpackets in order by the subpacket type identifier octet
         sig._signature.subpackets._normalize()
