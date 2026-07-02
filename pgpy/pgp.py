dkg/PGPy

commit 9cd3066b9a15ba31ed5b7d3cc8acca77821a0354

Allow setting subkey expiration

pgpy/pgp.py
@@ -2695,6 +2695,13 @@ class PGPKey(Armorable, ParentRef):
             if usage is not None:
                 sig._signature.subpackets.addnew('KeyFlags', hashed=True, critical=True, flags=usage)
 
+            key_expires = prefs.pop('key_expiration', None)
+            if key_expires is not None:
+                # key expires should be a timedelta, so if it's a datetime, turn it into a timedelta
+                if isinstance(key_expires, datetime):
+                    key_expires = key_expires - key.created
+                sig._signature.subpackets.addnew('KeyExpirationTime', hashed=True, expires=key_expires)
+
             crosssig = None
             # if possible, have the subkey create a primary key binding signature
             if key.key_algorithm is not None and key.key_algorithm.can_sign and prefs.pop('crosssign', True):

commit 04402ef46e869682a3ebfeb871add3076411ceed

Key Expiration Time subpacket SHOULD be marked as critical
see: https://www.rfc-editor.org/info/rfc9580/#section-5.2.3.13

Thanks for catching this, link2xt!

fixes: 9cd3066b9a15ba31ed5b7d3cc8acca77821a0354

pgpy/pgp.py
@@ -2700,7 +2700,7 @@ class PGPKey(Armorable, ParentRef):
                 # key expires should be a timedelta, so if it's a datetime, turn it into a timedelta
                 if isinstance(key_expires, datetime):
                     key_expires = key_expires - key.created
-                sig._signature.subpackets.addnew('KeyExpirationTime', hashed=True, expires=key_expires)
+                sig._signature.subpackets.addnew('KeyExpirationTime', hashed=True, critical=True, expires=key_expires)
 
             crosssig = None
             # if possible, have the subkey create a primary key binding signature